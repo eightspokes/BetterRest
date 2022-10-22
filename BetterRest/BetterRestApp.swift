d8107a3a55b7721e7310c4a8b8524b9e16a87e49
romanmuni8 <romanmuni8@gmail.com>
2022-10-22 13:40:26 -0400

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Roman on 10/22/22.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
