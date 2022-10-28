8e2d49da3ca974f9384c70a565b4252a93e3f6cb
romanmuni8 <romanmuni8@gmail.com>
2022-10-28 19:32:48 -0400

    MVP completed

--- BetterRest/ContentView.swift
+++ BetterRest/ContentView.swift
@@ -4,27 +4,22 @@
 //
 //  Created by Roman on 10/22/22.
 //
-
+import CoreML
 import SwiftUI
 struct Header : ViewModifier{
     func body(content: Content)-> some View{
         content
             .font(.footnote.bold())
-            .foregroundColor(.white)
-            
+            .foregroundColor(.black)
+        
     }
 }
-
-//We can not use it as .modifier(Title())
-//Or wecan make an extension
 
 extension View {
     func sectionHeader() -> some View {
         modifier(Header())
     }
 }
-// We can now use it by calling the menthod .titleStyle()
-
 
 struct NavAppearenceModifier: ViewModifier{
     init(backgroundColor: UIColor?, foreGroundColor: UIColor, tintColor: UIColor?, hideSeparator: Bool){
@@ -32,7 +27,7 @@ struct NavAppearenceModifier: ViewModifier{
         navBarAppearance.titleTextAttributes = [.foregroundColor : foreGroundColor]
         navBarAppearance.largeTitleTextAttributes = [.foregroundColor: foreGroundColor]
         navBarAppearance.configureWithTransparentBackground()
-
+        
         UINavigationBar.appearance().standardAppearance = navBarAppearance
         UINavigationBar.appearance().compactAppearance = navBarAppearance
         UINavigationBar.appearance().scrollEdgeAppearance = navBarAppearance
@@ -62,73 +57,92 @@ extension View {
 }
 
 struct ContentView: View {
-    @State private var wakeUp = Date.now
+    
+    
+    @State private var wakeUp = defaultWakeTime
     @State private var sleepAmount = 8.0
     @State private var coffeeAmount = 1
     
+    @State private var alertTitle = ""
+    @State private var alertMessage = "Set the parameters above"
+    @State private var showingAlert = false
     
-    
-    
+    static var defaultWakeTime : Date{
+        var components = DateComponents()
+        components.hour = 7
+        components.minute = 0
+        return Calendar.current.date(from: components) ?? Date.now
+    }
     
     var body: some View{
-        
-       
-            NavigationView{
-                VStack{
+               
+        NavigationView{
+            Form{
+                Section(header: Text("Wake up time").sectionHeader()){
+                    DatePicker("Enter a wake up time", selection: $wakeUp, displayedComponents: .hourAndMinute)
                     
-                    Form{
-                        
-                        Section(header: Text("Wake up time").sectionHeader()){
-                            DatePicker("Enter a wake up time", selection: $wakeUp, displayedComponents: .hourAndMinute)
-                                
-                                .frame(width: 310, height: 40, alignment: .trailing)
+                        .frame(width: 340, height: 20, alignment: .trailing)
+                        .pickerStyle(InlinePickerStyle())
+                        .onChange(of: wakeUp){ newValue in
+                            calculateBedtime()
                         }
-                           
-                        
-                        Section(header: Text("Sleep amount").sectionHeader()){
-                            Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
+                }
+                
+                Section(header: Text("Sleep amount").sectionHeader()){
+                    Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
+                        .onChange(of: sleepAmount){ newValue in
+                            calculateBedtime()
                         }
-                        
-                        Section(header: Text("Daily coffee intake").sectionHeader()){
-                            Stepper(coffeeAmount == 1 ? " 1 cup" : "\(coffeeAmount) cups", value: $coffeeAmount, in: 1...20)
-                        }
-                        
-                    }
-                    .onAppear {
-                        UITableView.appearance().backgroundView = UIImageView(image: UIImage(named: "coffee"))
-                    }
-                    
-                }.navigationBarTitle(Text("Better Rest")).foregroundColor(.white)
-                    .navigationAppearance(foreGroundColor: .white, hideSeparator: true )
-            }
+                }
 
+                Section(header: Text("Daily coffee intake").sectionHeader()){
+                    Picker("Daily coffee intake", selection: $coffeeAmount) {
+                        ForEach(1..<8) {
+                            Text("\($0)")
+                        }
+                    }.onChange(of: coffeeAmount){ newValue in
+                        calculateBedtime()
+                    }
+                }
+                Section(header: Text("Recomended bed time: ").font(.footnote.bold()).foregroundColor(.red)){
+                    
+                    Text(alertMessage)
+                }
+            }
+            //.onAppear {
+            // UITableView.appearance().backgroundView = UIImageView(image: UIImage(named: "coffee"))
+            //}
+            
+            .navigationBarTitle(Text("Better Rest").foregroundColor(.black))
+            .navigationAppearance(foreGroundColor: .black, hideSeparator: true )
+            .alert(alertTitle, isPresented: $showingAlert){
+                Button("OK"){}
+            }message:{
+                Text(alertMessage)
+            }
+        }
+    }
+    func calculateBedtime()  {
+        do {
+            let config = MLModelConfiguration()
+            let model = try SleepCalculator(configuration: config)
+            let components = Calendar.current.dateComponents([.hour,.minute], from: wakeUp)
+            let hour = (components.hour ?? 0) * 60 * 60
+            let minute = (components.minute ?? 0) * 60
+            let prediction = try model.prediction(wake: Double(hour + minute), estimatedSleep: sleepAmount, coffee: Double(coffeeAmount))
+            let sleepTime = wakeUp - prediction.actualSleep
+            
+            alertTitle = "Your ideal bedtime is..."
+            alertMessage = sleepTime.formatted(date:.omitted, time:.shortened)
+            
+        }catch{
+            alertTitle = "Error"
+            alertMessage = "Sorry, there is a problem calculation your bedtime"
+        }
+        //showingAlert = true
         
     }
 }
-
-
-
-//    .background(Image("coffee")
-//        .resizable())
-//    .navigationTitle("Better Rest")
-    
-//       Form{
-//           Stepper(String(format: "%.1f",sleepAmount), value: $sleepAmount, in: 4...12,step: 0.5)
-//           DatePicker("Please enter a date", selection: $wakeUp,in: Date.now..., displayedComponents: .hourAndMinute)
-//              // .labelsHidden()
-
-//    func trivialExample(){
-//        let components = Calendar.current.dateComponents([.hour,.minute], from: Date.now)
-//        let hour = components.hour ?? 0
-//        let minutes = components.minute ?? 0
-//
-//    }
-    
-//    func exampleDate(){
-//        let tomorrow = Date.now.addingTimeInterval(86400)
-//        let range = Date.now ... tomorrow
-//    }
-    
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
